3c01aaf98ebe7ae56f43ef968929ac353867ef07
RezHackXYZ <rhythmupa@gmail.com>
2025-05-29 16:25:32 +0530

    new names?

--- src/routes/kahootclone/+page.svelte
+++ src/routes/kahootclone/+page.svelte
@@ -1,6 +1,6 @@
 <div class="bg-grey-900 flex h-full items-center justify-center">
 	<div class="flex flex-col items-center justify-center gap-1 rounded-lg bg-gray-900 p-8 shadow-lg">
-		<h1 class="m-[0] text-6xl">DaKahootClone</h1>
+		<h1 class="m-[0] text-6xl">Quizlet</h1>
 		<p class="m-[0] mb-2 text-lg text-gray-400">The best ever kahoot clone.</p>
 		<a href="./kahootclone/join">
 			<button
