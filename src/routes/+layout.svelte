0e75609668e934a94fbbb0ecc80b4d7034e2732b
RezHackXYZ <rhythmupa@gmail.com>
2025-05-31 08:38:04 +0530

    added nav

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,5 @@
 <script>
+	import Nav from "./nav.svelte";
 	import "./tailwind.css";
 
 	import { Toaster } from "svelte-5-french-toast";
@@ -6,4 +7,16 @@
 </script>
 
 <Toaster />
-<div class="h-full font-[Sour_Gummy]">{@render children()}</div>
+<div class="h-full">
+	<!--
+	will be used for the nav bar later as i asked my friend to 
+	make a design for it and the whole app so ill make it 
+	functional once he gives me the design
+
+	<div class="m-3 flex w-[calc(100%-1.5rem)] items-center justify-between rounded border-2 p-1">
+		<Nav />
+	</div>
+	-->
+
+	{@render children()}
+</div>
---
+++ src/routes/nav.svelte
@@ -0,0 +1,13 @@
+<button class="btn compact dull text-4xl">
+	<img
+		src="https://hc-cdn.hel1.your-objectstorage.com/s/v3/4a82e0c815624c7786ca2a5addbcc74487da8940_group_8__2_.svg"
+		class="h-12"
+		alt=""
+	/>Go back
+	<i class="nf nf-md-keyboard_backspace"></i>
+</button>
+<div>
+	<button class="btn compact dull circular text-4xl" aria-label="Settings">
+		<i class="nf nf-seti-config"></i>
+	</button>
+</div>
