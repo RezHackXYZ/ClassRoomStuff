f1675d0c6bdc5a56f0f7992fab43a64f4cce9951
RezHackXYZ <rhythmupa@gmail.com>
2025-05-31 11:53:13 +0530

    fixed wrong link

--- src/routes/+HomePage/footer.svelte
+++ src/routes/+HomePage/footer.svelte
@@ -3,7 +3,7 @@
 		Made By <a href="https://rezhack.xyz" class="text-blue-400 underline">RezHackXYZ</a> for
 		<a href="https://neighborhood.hackclub.com/" class="text-blue-400 underline">Neighborhood</a>. •
 		Source code available
-		<a href="https://neighborhood.hackclub.com/" class="text-blue-400 underline">here</a>.
+		<a href="https://github.com/RezHackXYZ/ClassRoomStuff/" class="text-blue-400 underline">here</a>.
 	</div>
 	<div class="text-sm">
 		and the "<a href="?ascii" class="text-blue-400 underline">?ascii</a>" flag for a surprise!
