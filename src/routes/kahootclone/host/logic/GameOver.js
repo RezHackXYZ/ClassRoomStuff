4249f62cc51950935eebb512dd8770722bb78492
RezHackXYZ <rhythmupa@gmail.com>
2025-06-02 20:31:04 +0530

    remove old host file

--- src/routes/kahootclone/host/logic/GameOver.js
+++
@@ -1,7 +0,0 @@
-import { supabase } from "$lib/supabase.js";
-
-export async function GameOver(GamePin) {
-	await supabase.from("games").update({ status: `completed` }).eq("gamepin", GamePin);
-
-	window.location.replace("/kahootclone/results?gamepin=" + GamePin + "&playerID=host-null");
-}
